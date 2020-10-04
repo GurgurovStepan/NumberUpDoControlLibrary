e6958bcc2f53c320814a0a7615905a9dcef8d676
Tiuba_HomePC <Tiuba@bk.ru>
2020-10-04 23:23:04 +0300

    Добавьте файлы проекта.

---
+++ Logics.cs
@@ -0,0 +1,71 @@
+﻿using System.Net;
+using System.Windows.Converters;
+using System.Windows.Data;
+
+namespace NumberUpDoControlLibrary
+{
+    class Logics
+    {
+        /// <summary>
+        /// Текущее значение
+        /// </summary>
+        private int curentValue;
+        /// <summary>
+        /// максимальное значение
+        /// </summary>
+        private readonly int maxValue = 100;
+        /// <summary>
+        /// минимальное значение
+        /// </summary>
+        private readonly int minValue = -100;
+        /// <summary>
+        /// Текущее значение
+        /// </summary>
+        public int Value 
+        {
+            get 
+            {
+                if (curentValue > maxValue)
+                    return maxValue;
+
+                if (curentValue < minValue)
+                    return minValue; 
+                
+                return curentValue;
+
+            }
+            protected set { curentValue = value; }
+        }
+        /// <summary>
+        /// Увеличить текущее значение
+        /// </summary>
+        public int UpValue
+        {
+            get
+            { 
+                Value += 5;
+                return Value;
+            }
+        }
+       /// <summary>
+       /// уменьшить текущее значение
+       /// </summary>
+        public int DownValue
+        {
+            get 
+            { 
+                Value -= 5;
+                return Value;
+            }
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Logics() 
+        { 
+            Value = 0; 
+        }
+        
+    }
+}
---
+++ UserControl1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberUpDoControlLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            field.Text = logic.Value.ToString();
+        }
+
+        Logics logic = new Logics();
+        private void up_Click(object sender, RoutedEventArgs e)
+        {
+           int temp = logic.UpValue;
+            field.Text = temp.ToString();
+        }
+
+        private void down_Click(object sender, RoutedEventArgs e)
+        {
+            int temp = logic.DownValue;
+            field.Text = temp.ToString();
+        }
+    }
+}
